c71bcfd40b2d2801d263895980349cb9d12566b4
nzfreeman <51111083+nzfreeman@users.noreply.github.com>
2020-05-11 12:32:48 +1200

    데이터저장예시

---
+++ scripts/getdatafromFirestore.js
@@ -0,0 +1,21 @@
+<input type = "text" id = "booking_list" >
+  <script>
+  function getDatafromFirestore() {
+    var docRef = db.collection(year).doc(month).collection(day).doc(table_number);
+    docRef.get().then(function(doc) {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }).catch(function(error) {
+      console.log("Error getting document:", error);
+    });
+
+    docRef.document.getElementById("booking_list").innerHTML;
+
+  }
+
+  <
+  /script>
